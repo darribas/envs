720e21f4aed92a9c3d4c0d27077bddf4d7de5c0f
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-03-01 14:47:00 +0000

    Moving all packages geo to IOOS channel in pydata

--- pydata.sh
+++ pydata.sh
@@ -7,32 +7,34 @@ conda create -y -n pydata ipython=4.1 jupyter=1.*
 conda install -n pydata -y --no-update-deps \
     'pip' \
     'dask=0.8*' \
-    'pandas=0.16*' \
-    'numpy=1.9*' \
+    'pandas=0.17*' \
+    'numpy=1.10*' \
     'psycopg2' \
     'sqlalchemy=1.0.12' \
-    'fiona=1.5' \
-    'gdal=1.11*' \
     'six' \
-    'rasterio' \
     'pytables=3.2*' \
-    'shapely=1.5.8' \
-    'pyproj=1.9.4' \
-    'matplotlib=1.4*' \
+    'matplotlib=1.5*' \
     'seaborn=0.7' \
     'bokeh=0.11' \
     'ipywidgets=4.1*' \
     'scipy=0.17' \
     'statsmodels=0.6*' \
     'networkx=1.11' \
-    'scikit-learn=0.16*' \
+    'scikit-learn=0.16*'
+
+conda install -c ioos -n pydata -y --no-update-deps \
+    'fiona=1.6*' \
+    'gdal=1.11*' \
+    'rasterio=0.31' \
+    'shapely=1.5.8' \
+    'pyproj=1.9.4' \
     'krb5'
 
 conda install -y -n pydata -c anaconda-nb-extensions nbpresent nbbrowserpdf
 
 source activate pydata
 
-pip install -U geopy descartes clusterpy mplleaflet brewer2mpl
+pip install -U geopy descartes clusterpy mplleaflet brewer2mpl clusterpy
 pip install git+https://github.com/quantopian/qgrid.git@v0.3.0
 pip install -U --no-deps pysal==1.11.0
 
