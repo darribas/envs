60aee6f7dfd200ddb421145748613f36137b5730
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-03-03 09:20:51 +0000

    Fixing pydata_exp, now works on linux

--- pydata_exp.sh
+++ pydata_exp.sh
@@ -26,7 +26,7 @@ conda install -c ioos -n pydata_exp -y --no-update-deps \
     'fiona=1.6*' \
     'gdal=1.11*' \
     'rasterio=0.31' \
-    'shapely=1.5.8' \
+    'shapely=1.5*' \
     'pyproj=1.9.4' \
     'krb5'
 
@@ -34,9 +34,14 @@ conda install -y -n pydata_exp -c anaconda-nb-extensions nbpresent nbbrowserpdf
 
 source activate pydata_exp
 
-pip install -U geopy descartes clusterpy mplleaflet brewer2mpl clusterpy
+pip install -U geopy descartes clusterpy mplleaflet brewer2mpl
 pip install git+https://github.com/quantopian/qgrid.git@v0.3.0
 pip install -U --no-deps pysal==1.11.0
 
 pip install -U --no-deps git+git://github.com/geopandas/geopandas.git
+
+rm -f pydata_test.html
+jupyter nbconvert --to notebook --execute --allow-errors pydata_check.ipynb --output pydata_test.ipynb
+jupyter nbconvert --to html pydata_test.ipynb
+rm pydata_test.ipynb
 
