7ebd601e695ea5469c2d5ac6b75054e7affff0ba
Dani Arribas-Bel <daniel.arribas.bel@gmail.com>
2016-06-01 09:05:49 +0100

    Adding pydata3 env with Python 3.5

---
+++ pydata3.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+conda-env remove -y -n pydata3
+
+conda create -y -n pydata3 python=3.5.*
+
+conda install -c conda-forge -n pydata3 -y \
+    'bokeh=0.11' \
+    'cartopy=0.14*' \
+    'cython=0.23.*' \
+    'dask=0.8*' \
+    'fiona=1.6*' \
+    'gdal=1.11*' \
+    'ipython=4.2.*' \
+    'ipywidgets=4.1*' \
+    'jupyter=1.*' \
+    'krb5' \
+    'libgdal=1.11*' \
+    'networkx=1.11' \
+    'numpy=1.10*' \
+    'matplotlib=1.5*' \
+    'pandas=0.18*' \
+    'pip' \
+    'psycopg2' \
+    'pyproj=1.9.4' \
+    'pytables=3.2*' \
+    'rasterio=0.32*' \
+    'seaborn=0.7*' \
+    'shapely=1.5*' \
+    'six' \
+    'scipy=0.17' \
+    'scikit-learn=0.17*' \
+    'sqlalchemy=1.0.12' \
+    'statsmodels=0.6*' \
+    'xlrd=0.9*' \
+    'xlsxwriter=0.8.*'
+
+conda install -y -n pydata3 -c anaconda-nb-extensions nbpresent nbbrowserpdf
+
+source activate pydata3
+
+pip install -U --no-deps geopy descartes clusterpy mplleaflet brewer2mpl rpy2
+pip install -U --no-deps git+https://github.com/quantopian/qgrid.git@v0.3.0
+pip install -U --no-deps pysal==1.11.1
+
+pip install -U --no-deps git+git://github.com/geopandas/geopandas.git@master
+
+rm -f pydata_test.html
+jupyter nbconvert --to notebook --execute --allow-errors pydata_check.ipynb --output pydata_test.ipynb
+jupyter nbconvert --to html pydata_test.ipynb
+rm pydata_test.ipynb
+
